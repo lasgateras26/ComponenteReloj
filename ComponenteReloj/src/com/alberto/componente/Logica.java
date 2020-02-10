5311b5fbd861a192d86c922d1ed36d6b4bbf50b7
lasgateras26 <lasgateras2@gmail.com>
2020-02-10 12:13:04 +0100

    logica añadida, falta comprobar el evento

---
+++ ComponenteReloj/src/com/alberto/componente/Logica.java
@@ -0,0 +1,33 @@
+package com.alberto.componente;
+
+import com.alberto.tareas.Tarea;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logica {
+
+    private static Logica INSTANCE = null;
+    private List<Tarea> listaTareas = new ArrayList<>();
+
+    private Logica(){
+    }
+
+    public static Logica getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new Logica();
+        return INSTANCE;
+    }
+
+    public List<Tarea> getListaTareas() {
+        return listaTareas;
+    }
+
+    public void setListaTareas(List<Tarea> listaTareas) {
+        this.listaTareas = listaTareas;
+    }
+
+    public void añadirTarea(Tarea tarea){
+        listaTareas.add(tarea);
+    }
+}
--- ComponenteReloj/src/com/alberto/tareas/Tarea.java
+++ ComponenteReloj/src/com/alberto/tareas/Tarea.java
@@ -1,30 +1,71 @@
 package com.alberto.tareas;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Tarea {
 
-    private String texto;
-    private LocalDateTime fecha;
+    private String nombre;
+    private LocalDate fecha;
+    private int segundos;
+    private int minutos;
+    private int hora;
 
-    public Tarea(String texto, LocalDateTime fecha) {
-        this.texto = texto;
+    public Tarea(String nombre, LocalDate fecha, int segundos, int minutos, int hora) {
+        this.nombre = nombre;
         this.fecha = fecha;
+        this.segundos = segundos;
+        this.minutos = minutos;
+        this.hora = hora;
     }
 
-    public String getTexto() {
-        return texto;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setTexto(String texto) {
-        this.texto = texto;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public LocalDateTime getFecha() {
+    public LocalDate getFecha() {
         return fecha;
     }
 
-    public void setFecha(LocalDateTime fecha) {
+    public void setFecha(LocalDate fecha) {
         this.fecha = fecha;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "nombre='" + nombre + '\'' +
+                ", fecha=" + fecha +
+                ", segundos=" + segundos +
+                ", minutos=" + minutos +
+                ", hora=" + hora +
+                '}';
     }
 }
